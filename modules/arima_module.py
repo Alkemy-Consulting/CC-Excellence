2c95bed263863bdb0fae6b1928c52207a8fb6524
Filo93 <filo.raimondi@gmail.com>
2025-06-27 09:31:08 +0000

    Aggiunta modello Holt-Winters e placeholder per ARIMA/SARIMA

--- modules/arima_module.py
+++ modules/arima_module.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+def run_arima_model(*args, **kwargs) -> None:
+    """Placeholder runner to be used from the Streamlit app."""
+    st.info("\ud83d\udd27 Modulo ARIMA in sviluppo. Presto disponibile.")
